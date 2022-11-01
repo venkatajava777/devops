78c2505969b8ccf296cbefa57aea1eeb822b6926
venkatajava777 <116952191+venkatajava777@users.noreply.github.com>
2022-11-01 19:02:19 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Addition{
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter a,b=");
+		int a=input.nextInt();
+		int b=input.nextInt();
+		int c=a+b;
+		System.out.println("Addition of "+a+" and "+b+"="+c);
+	}
+}	
+		
