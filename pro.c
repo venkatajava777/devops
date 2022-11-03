8617e706c310ca09bd88afb8b100fced49ba25fe
venkatajava777 <cvlnaits@gmail.com>
2022-11-03 17:09:28 +0530

    firrst

---
+++ pro.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i;
+	char a[20];
+	printf("enter the name");
+	for(i=0;i<10;i++)
+	{
+	  scanf("%c",&a[i]);
+   }
+   for(i=0;i<5;i++)
+   {
+	printf("%c",a[i]);
+   }
+	return 0;
+
+}
+
